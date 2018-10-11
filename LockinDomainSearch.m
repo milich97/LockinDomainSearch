eb85c75c8b614f26a2cb48302696ee083523d9c6
milich97 <misha199764@gmail.com>
2018-10-11 12:51:10 +0300

    Update LockinDomainSearch.m

--- LockinDomainSearch.m
+++ LockinDomainSearch.m
@@ -4,8 +4,14 @@ clc;
 tau_1=0.5;
 tau_2=0.25;
 omega_e_free=0;
+k_vco=1;
 
+PLLSys = @(t, z, tau_1, tau_2, omega_e_free)([omega_e_free - k_vco * (...
+    1 / tau_1 * z(2) + (tau_1 * tau_2 - 1) / tau_1 * z(3) + sin(z(1))...
+    ),sin(z(1)),- tau_1 * z(3) + sin(z(1))]');
 sys = @(t,z) PLLSys(t, z, tau_1, tau_2, omega_e_free);
+Event = @(t, z, initial_theta_e)deal([1*((abs(z(1) - round(z(1)/2/pi)*2*pi) ...
+    + abs(z(2)) + abs(z(3)) >= 1.e-4) && (abs(initial_theta_e - z(1)) >= 2*pi)),1,0]);
 len = 20;
 %xoverFcn = @(t, z) Event(t, z);
 %options = odeset('RelTol', 1.e-3, 'AbsTol', 1.e-3, 'events', xoverFcn);
@@ -18,6 +24,7 @@ for x_1_initial=-3.1:step:3
     for x_2_initial=-6:step:6.2
         for theta_e_initial=-pi:step:pi
             min=0;max=0;
+            
             xoverFcn = @(t, z) Event(t, z, theta_e_initial);
             options = odeset('RelTol', 1.e-3, 'AbsTol', 1.e-3, 'events', xoverFcn);
             [T,Y] = ode15s(sys, [0 len], [theta_e_initial x_1_initial x_2_initial], options);
@@ -46,52 +53,3 @@ ylabel('\textbf{$x_1$}','Interpreter','latex', 'fontsize',20,'rotation',0);
 zlabel('\textbf{$x_2$}','Interpreter','latex', 'fontsize',20,'rotation',0);
 grid on;
 clear T Y;
-
-function [value,isterminal,direction] = Event( t, z, initial_theta_e)
-
-% 1 - theta_e
-% 2 - x_1
-% 3 - x_2
-theta_e = z(1);
-x1 = z(2);
-x2 = z(3);
-is_in_equilibria = abs(theta_e - round(theta_e/2/pi)*2*pi) ...
-    + abs(x1) + abs(x2) < 1.e-4;
-is_cycle_slipping = abs(initial_theta_e - theta_e) > 2*pi;
-if (is_in_equilibria || is_cycle_slipping)
-    value = 0;
-else 
-    value = 1;
-end
-isterminal=1;
-direction=0;
-
-end
-
-function dz = PLLSys(t, z,tau_1,tau_2,omega_e_free)
-
-dz = zeros(3,1);
-
-% 1 - theta_e
-% 2 - x_1
-% 3 - x_2
-theta_e = z(1);
-x1 = z(2);
-x2 = z(3);
-
-
-%TODO pass this from arguments
-k_vco = 1;
-
-dx1 = sin(theta_e);
-dx2 = - tau_1 * x2 + sin(theta_e);
-dtheta_e = omega_e_free - k_vco * (...
-    1 / tau_1 * x1 + (tau_1 * tau_2 - 1) / tau_1 * x2 + sin(theta_e)...
-    );
-
-dz(1) = dtheta_e;
-dz(2) = dx1;
-dz(3) = dx2;
-
-end
-
